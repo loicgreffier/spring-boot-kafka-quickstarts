0aaf7e66e9d69864088c7ee9fe31635e21a21f93
Loïc Greffier <loic.greffier@michelin.com>
2025-01-07 14:55:45 +0100

    Fix missing license header

--- kafka-streams-quickstarts/kafka-streams-store-window/src/test/java/io/github/loicgreffier/streams/store/window/KafkaStreamsStoreWindowApplicationTest.java
+++ kafka-streams-quickstarts/kafka-streams-store-window/src/test/java/io/github/loicgreffier/streams/store/window/KafkaStreamsStoreWindowApplicationTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.github.loicgreffier.streams.store.window;
 
 import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
@@ -7,7 +26,6 @@ import static io.github.loicgreffier.streams.store.window.constant.Topic.PERSON_
 import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
 import static org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG;
 import static org.apache.kafka.streams.StreamsConfig.STATE_DIR_CONFIG;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
