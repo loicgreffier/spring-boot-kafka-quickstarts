0f3b4b3a3cab044787b2b09bc3a74c9e9d39b053
Loïc Greffier <loic.greffier@hotmail.fr>
2025-11-22 22:53:57 +0100

    Bump to Java 25

--- kafka-streams-quickstarts/kafka-streams-left-join-stream-stream/src/main/java/io/github/loicgreffier/streams/leftjoin/stream/stream/app/KafkaStreamsTopology.java
+++ kafka-streams-quickstarts/kafka-streams-left-join-stream-stream/src/main/java/io/github/loicgreffier/streams/leftjoin/stream/stream/app/KafkaStreamsTopology.java
@@ -42,26 +42,6 @@ import org.apache.kafka.streams.kstream.StreamJoined;
 public class KafkaStreamsTopology {
 
     /**
-     * Builds the Kafka Streams topology. The topology reads from the USER_TOPIC topic and the USER_TOPIC_TWO topic. The
-     * stream is joined to the other stream by last name with a left join and 5-minute symmetric join windows with
-     * 1-minute grace period. The result is written to the USER_LEFT_JOIN_STREAM_STREAM_TOPIC topic.
-     *
-     * <p>A left join emits an output for each record in the primary stream. If there is no matching record in the
-     * secondary stream, a null value is returned at the end of the join window + grace period. A new record to process
-     * is required to make the stream time advance and emit the null result.
-     *
-     * <p>{@link JoinWindows} are aligned to the records timestamp. They are created each time a record is processed and
-     * are bounded such as [timestamp - before, timestamp + after]. An output is produced if a record is found in the
-     * secondary stream that has a timestamp within the window of a record in the primary stream such as:
-     *
-     * <pre>
-     * {@code stream1.ts - before <= stream2.ts AND stream2.ts <= stream1.ts + after}
-     * </pre>
-     *
-     * @param streamsBuilder The streams builder.
-     */
-
-    /**
      * Builds the Kafka Streams topology. The topology reads from the {@code USER_TOPIC} topic and the
      * {@code USER_TOPIC_TWO} topic. The stream is joined to the other stream by last name using a left join and
      * 5-minute symmetric join windows with a 1-minute grace period. The result is written to the
