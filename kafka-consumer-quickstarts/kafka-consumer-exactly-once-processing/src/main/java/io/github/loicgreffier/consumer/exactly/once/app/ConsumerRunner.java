876b0c7901fab2c790dccc4535e519fc210ff4fb
Loïc Greffier <loic.greffier@michelin.com>
2026-07-20 16:51:46 +0200

    Reflow exactly-once processing Javadoc line wrapping

--- kafka-consumer-quickstarts/kafka-consumer-exactly-once-processing/src/main/java/io/github/loicgreffier/consumer/exactly/once/app/ConsumerRunner.java
+++ kafka-consumer-quickstarts/kafka-consumer-exactly-once-processing/src/main/java/io/github/loicgreffier/consumer/exactly/once/app/ConsumerRunner.java
@@ -73,8 +73,8 @@ public class ConsumerRunner {
      * <p>Transactions ensure that processed records are sent to the output topic along with the offsets of the
      * partitions that were processed. This guarantees exactly-once processing, meaning that for each record received,
      * its processed results will be reflected once, even in the event of failures. Without transactions, a failure
-     * occurring after some records have been sent but before the offsets are committed could lead to duplicate
-     * records in the output topic, as the entire batch would be re-processed on restart.
+     * occurring after some records have been sent but before the offsets are committed could lead to duplicate records
+     * in the output topic, as the entire batch would be re-processed on restart.
      */
     @Async
     @EventListener(ApplicationReadyEvent.class)
